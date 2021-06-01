39a836ca559ecb6c4502f3e487973c050f6eaa19
Renz Isla <renz.mbisla@gmail.com>
2021-06-01 17:00:18 +0800

    Initial project creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChromediaRestApiExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
